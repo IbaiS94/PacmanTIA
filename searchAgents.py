dc269577c1d566bf3aa09e3622e05cca27fcb5f8
Ibai Sologestoa <98406330+IbaiS94@users.noreply.github.com>
2024-10-09 21:14:41 +0200

    se me olvido poner el codigo antiguo

--- searchAgents.py
+++ searchAgents.py
@@ -574,7 +574,46 @@ def foodHeuristic(state, problem):
     
     return dis_haciacomida + dist_maxentrecomida
 
+"""def foodHeuristic(state, problem):
+    
+    Your heuristic for the FoodSearchProblem goes here.
 
+    This heuristic must be consistent to ensure correctness.  First, try to come
+    up with an admissible heuristic; almost all admissible heuristics will be
+    consistent as well.
+
+    If using A* ever finds a solution that is worse uniform cost search finds,
+    your heuristic is *not* consistent, and probably not admissible!  On the
+    other hand, inadmissible or inconsistent heuristics may find optimal
+    solutions, so be careful.
+
+    The state is a tuple ( pacmanPosition, foodGrid ) where foodGrid is a Grid
+    (see game.py) of either True or False. You can call foodGrid.asList() to get
+    a list of food coordinates instead.
+
+    If you want access to info like walls, capsules, etc., you can query the
+    problem.  For example, problem.walls gives you a Grid of where the walls
+    are.
+
+    If you want to *store* information to be reused in other calls to the
+    heuristic, there is a dictionary called problem.heuristicInfo that you can
+    use. For example, if you only want to count the walls once and store that
+    value, try: problem.heuristicInfo['wallCount'] = problem.walls.count()
+    Subsequent calls to this heuristic can access
+    problem.heuristicInfo['wallCount']
+    
+    position, foodGrid = state
+    foodList = foodGrid.asList()  
+    paredes = problem.walls.asList()
+    distancia = []
+    if not foodList:
+        return 0
+    
+    for food in foodList:
+        distancia.append(abs(position[0] - food[0]) + abs(position[1] - food[1]))
+    
+    
+    return max(distancia) """
 class ClosestDotSearchAgent(SearchAgent):
     """Search for all food using a sequence of searches"""
 
